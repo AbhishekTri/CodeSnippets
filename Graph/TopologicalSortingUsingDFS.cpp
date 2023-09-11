e6a16a340761ecb00c2f0fc4c820e1134b46064b
Abhishek Tripathi <88419578+AbhishekTri@users.noreply.github.com>
2023-09-11 23:15:25 +0530

    Create TopologicalSortingUsingDFS.cpp

---
+++ Graph/TopologicalSortingUsingDFS.cpp
@@ -0,0 +1,13 @@
+void topoSortDfs(int src, unordered_map<int, bool>& visited, stack<int>& ans) {
+
+  visited[src] = true;
+
+  for(auto neighbour: adjList[src]) {
+   if(!visited[neighbour] ) {
+    topoSortDfs(neighbour, visited, ans);
+   }
+  }
+
+  //while returning, store the node in stack
+  ans.push(src);
+ }
